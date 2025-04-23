87cde8651b3532bd750ba86500e2a40756d10b3a
projectpolaris08 <barallasab@outlook.com>
2025-04-23 18:59:43 +0800

    Fix page is not rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { Layout } from './components/Layout';
 import { Hero } from './components/Hero';
 import { Stats } from './components/Stats';
 import { Features } from './components/Features';
-import { Calculator } from './components/Calculator';
+import Calculator from './components/Calculator';
 import { Services } from './components/Services';
 import { ContactForm } from './components/ContactForm';
 import { Footer } from './components/Footer';
