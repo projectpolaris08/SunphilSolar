309310b03d0e8f31c85ba6c307d471b4b15d9bc8
projectpolaris08 <barallasab@outlook.com>
2025-05-31 12:01:54 +0800

    make adjustments

--- solar-backend/api/news.js
+++ solar-backend/api/news.js
@@ -1,10 +1,9 @@
-const fetch = require("node-fetch");
-
 module.exports = async function handler(req, res) {
   const apiKey = process.env.NEWSAPI_KEY;
   const url = `https://eventregistry.org/api/v1/article/getArticles?apiKey=${apiKey}&resultType=articles&articlesPage=1&articlesCount=3&articlesSortBy=date&articlesSortByAsc=false&articleBodyLen=300&lang=eng`;
 
   try {
+    const fetch = (await import("node-fetch")).default;
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
